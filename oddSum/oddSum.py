8914f95175a06ba0a7ca6906d6d6d6581d7c3345
nicolas0p <nicolas0pfeifer@gmail.com>
2017-07-12 01:19:48 -0300

    oddSum accepted :D

--- oddSum/oddSum.py
+++ oddSum/oddSum.py
@@ -1,12 +1,38 @@
+import bisect
 
 def odd_sum(seq):
-    result = sum(seq)
-    return result
+    result = 0
+    min_pos_odd = False
+    max_neg_odd = False
+    for i in seq:
+        if(i < 0):
+            if i % 2 == 1:
+                if not bool(max_neg_odd) or i > max_neg_odd:
+                    max_neg_odd = i
+        else:
+            if i % 2 == 0: #even_pos
+                result += i
+            else: #odd_pos
+                result += i
+                if not bool(min_pos_odd) or i < min_pos_odd:
+                    min_pos_odd = i
+    if result % 2 == 1:
+        return result
+    minus_pos_odd = False
+    if bool(min_pos_odd):
+        minus_pos_odd = result - min_pos_odd #least positive odd
+    plus_neg_odd = False
+    if bool(max_neg_odd):
+        plus_neg_odd = result + max_neg_odd #least negative odd
+    if not bool(minus_pos_odd):
+        return plus_neg_odd
+    elif not bool(plus_neg_odd):
+        return minus_pos_odd
+    if minus_pos_odd > plus_neg_odd:
+        return minus_pos_odd
+    return plus_neg_odd
 
 if __name__ == "__main__":
-    n, q = input().split(' ')
-    result = kfactorization(int(n), int(q))
-    text = ""
-    for i in result:
-        text += str(i) + " "
-    print(text)
+    n = int(input())
+    seq = [int (x) for x in input().split(' ')]
+    print(odd_sum(seq))
--- oddSum/oddSum_test.py
+++ oddSum/oddSum_test.py
@@ -5,9 +5,19 @@ class oddSumTest(unittest.TestCase):
 
     def test1(self):
         num = [-2, 2, -3, 1]
-        #import pdb; pdb.set_trace()
         result = odd.odd_sum(num)
         self.assertEqual(3, result)
+
+    def test2(self):
+        num = [2, -5, -3]
+        #import pdb; pdb.set_trace()
+        result = odd.odd_sum(num)
+        self.assertEqual(-1, result)
+
+    def test3(self):
+        num = [-6004, 4882, 9052, 413, 6056, 4306, 9946, -4616, -6135, 906, -1718, 5252, -2866, 9061, 4046]
+        result = odd.odd_sum(num)
+        self.assertEqual(53507, result)
 
 if __name__ == '__main__':
     unittest.main()
